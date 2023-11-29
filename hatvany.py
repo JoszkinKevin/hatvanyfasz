47629812cf82c0b659a8244ee1bf41b3e378b687
JoszkinKevin <54977279+JoszkinKevin@users.noreply.github.com>
2023-11-29 15:55:10 +0100

    Update hatvany.py

--- hatvany.py
+++ hatvany.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+hatvanykitevo = st.slider(label'hatvany (2 ^ )', min_value=0, max_value = 16)
+
+st.write(2**hatvanykitevo)
+
+data = {
+    'x': [i**2 for i in range(hatvanykitevo)] }
+
+dataframe =pd.DataFrame(data)
+st.write("szex")
+st.line_chart(dataframe)
